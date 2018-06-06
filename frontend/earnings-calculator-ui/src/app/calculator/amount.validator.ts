411a53ffb915437d1fc1e8183a0ea089274cffd9
pawgh <t@t.t>
2018-06-06 23:07:15 +0200

    poprawa walidacji wprowadzonej kwoty

--- frontend/earnings-calculator-ui/src/app/calculator/amount.validator.ts
+++ frontend/earnings-calculator-ui/src/app/calculator/amount.validator.ts
@@ -2,6 +2,6 @@ import {AbstractControl, Validator, ValidatorFn} from "@angular/forms";
 
 export function amountFormatValidator(): ValidatorFn {
   return (control: AbstractControl): {[key: string]: any} | null => {
-    return (!(/^\d+\.?\d{2}$/.test(control.value))) ? {'amountFormat': {value: control.value}} : null;
+    return (!(/^\d+\.\d{2}$/.test(control.value))) ? {'amountFormat': {value: control.value}} : null;
   };
 }
